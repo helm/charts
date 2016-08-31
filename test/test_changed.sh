5e356f20bdf1742ca228568adee2b273b00f616f
Vic Iglesias <viglesias@google.com>
2016-08-31 00:02:12 -0700

    Use absolute path to helm

--- test/test_changed.sh
+++ test/test_changed.sh
@@ -3,5 +3,5 @@ UPSTREAM_BRANCH="upstream/master"
 
 CHANGED_FOLDERS=`git diff --name-only ${UPSTREAM_BRANCH} | grep -v test | grep / | uniq | awk -F/ '{print $2}'`
 for directory in ${CHANGED_FOLDERS}; do
-  "helm lint ${directory}"
+  "/opt/linux-amd64/helm lint ${directory}"
 done
