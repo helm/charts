118284e61e71bd88b6c6e9642e5517e0976b2b1e
Vic Iglesias <viglesias@google.com>
2016-08-30 23:42:37 -0700

    Get only chart name for changed charts

--- test/test_changed.sh
+++ test/test_changed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 UPSTREAM_BRANCH="upstream/master"
 
-CHANGED_FOLDERS=`git diff --name-only ${UPSTREAM_BRANCH} | grep -v test | grep / | uniq`
+CHANGED_FOLDERS=`git diff --name-only ${UPSTREAM_BRANCH} | grep -v test | grep / | uniq | awk -F/ '{print $2}'`
 for directory in ${CHANGED_FOLDERS}; do
   "helm lint ${directory}"
 done
