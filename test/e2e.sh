dbcea836773b053c5485e14f75e1dd1e163f37f4
Aaron Duke <dukuru@gmail.com>
2016-08-31 01:10:15 -0700

    Add license headers to test scripts

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,3 +1,17 @@
 #!/bin/bash -xe
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 docker run -v `pwd`:/src gcr.io/kubernetes-charts-ci/test-image:v1.4 /src/test/changed.sh
 echo "Done Testing!"
