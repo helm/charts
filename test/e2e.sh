164167361cca1116b697d3af0d9ba70bb98a28ff
Vic Iglesias <viglesias@google.com>
2016-08-30 23:28:01 -0700

    Update image

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
-docker run -v `pwd`:/src gcr.io/kubernetes-charts-ci/test-image:v1.1 /src/test/test_changed.sh
+docker run -v `pwd`:/src gcr.io/kubernetes-charts-ci/test-image:v1.2 /src/test/test_changed.sh
 echo "Done Testing!"
