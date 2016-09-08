7a3634b3155cfc6d6a68ba175f0272a0d06acf8f
Vic Iglesias <viglesiasce@gmail.com>
2016-09-08 12:55:35 -0700

    Upgrade helm to alpha.4 (#68)

--- test/e2e.sh
+++ test/e2e.sh
@@ -13,5 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-docker run -v `pwd`:/src gcr.io/kubernetes-charts-ci/test-image:v1.4 /src/test/changed.sh
+docker run -v `pwd`:/src gcr.io/kubernetes-charts-ci/test-image:v1.5 /src/test/changed.sh
 echo "Done Testing!"
