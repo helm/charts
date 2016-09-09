fe4bb8169e6c62dca0cc67a251c4fd48c32dbfa9
Vic Iglesias <viglesias@google.com>
2016-09-09 16:09:35 -0700

    Temporarily update helm to work around linter issue (#70)

--- test/publish-docker.sh
+++ test/publish-docker.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-VERSION=v1.4.1
+VERSION=v1.6
 CONTAINER_NAME=test-image
 
 usage() {
