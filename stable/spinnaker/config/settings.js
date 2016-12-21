ce3d0dad36908730de32f9cee4cbee9040ca9f53
Vic Iglesias <viglesiasce@gmail.com>
2016-12-21 09:18:50 +0000

    Jenkins patch

--- stable/spinnaker/config/settings.js
+++ stable/spinnaker/config/settings.js
@@ -5,47 +5,16 @@ var gateHost = 'http://localhost:8084';
 var bakeryDetailUrl = 'http://localhost:8087';
 
 window.spinnakerSettings = {
-  defaultProviders: ['aws', 'gce', 'azure', 'cf', 'kubernetes', 'titan'],
+  defaultProviders: ['kubernetes'],
   feedbackUrl: feedbackUrl,
   gateUrl: gateHost,
   bakeryDetailUrl: bakeryDetailUrl,
   pollSchedule: 30000,
-  defaultTimeZone: 'America/New_York', // see http://momentjs.com/timezone/docs/#/data-utilities/
+  defaultTimeZone: 'America/Los_Angeles', // see http://momentjs.com/timezone/docs/#/data-utilities/
   providers: {
-    azure: {
-      defaults: {
-        account: 'azure-test',
-        region: 'West US'
-      },
-    },
-    aws: {
-      defaults: {
-        account: 'test',
-        region: 'us-east-1'
-      },
-      defaultSecurityGroups: ['nf-datacenter-vpc', 'nf-infrastructure-vpc', 'nf-datacenter', 'nf-infrastructure'],
-      loadBalancers: {
-        // if true, VPC load balancers will be created as internal load balancers if the selected subnet has a purpose
-        // tag that starts with "internal"
-        inferInternalFlagFromSubnet: false,
-      },
-    },
-    gce: {
-      defaults: {
-        account: 'my-google-account',
-        region: 'us-central1',
-        zone: 'us-central1-f',
-      },
-    },
-    titan: {
-      defaults: {
-        account: 'titustest',
-        region: 'us-east-1'
-      },
-    },
     kubernetes: {
       defaults: {
-        account: 'my-kubernetes-account',
+        account: 'local-cluster',
         namespace: 'default'
       },
     }
@@ -55,14 +24,14 @@ window.spinnakerSettings = {
       enabled: true,
     },
     hipchat: {
-      enabled: true,
+      enabled: false,
       botName: 'Skynet T-800'
     },
     sms: {
-      enabled: true,
+      enabled: false,
     },
     slack: {
-      enabled: true,
+      enabled: false,
       botName: 'spinnakerbot'
     }
   },
@@ -73,12 +42,12 @@ window.spinnakerSettings = {
   feature: {
     pipelines: true,
     jobs: true,
-    notifications: false,
+    notifications: true,
     fastProperty: false,
     vpcMigrator: false,
     clusterDiff: false,
     roscoMode: false,
     netflixMode: false,
-    infrastructureStages: false, // Should 'createLoadBalancer' be a pipeline stage? (no).
+    infrastructureStages: true, // Should 'createLoadBalancer' be a pipeline stage? (no).
   },
 };
