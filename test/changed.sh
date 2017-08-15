f2c2c90184c3c0af0736db540fd00d19fb4a8aca
Vic Iglesias <viglesias@google.com>
2017-08-15 12:03:24 -0700

    Add helm test to CI (#1702)

--- test/changed.sh
+++ test/changed.sh
@@ -72,6 +72,7 @@ for directory in ${CHANGED_FOLDERS}; do
     kubectl get svc --namespace ${NAMESPACE}
     kubectl get deployments --namespace ${NAMESPACE}
     kubectl get endpoints --namespace ${NAMESPACE}
+    helm test
     if [ -n $VERIFICATION_PAUSE ]; then
       cat install_output
       sleep $VERIFICATION_PAUSE
