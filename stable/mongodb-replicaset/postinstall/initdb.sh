29be390b3543fff8a9911561f46970392a2db78f
Ben Mathews <ben@mathews2000.com>
2018-09-10 12:24:45 -0600

    removed extra files as requested

--- stable/mongodb-replicaset/postinstall/initdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-until mongo --host $MONGO_HOST -u $ADMIN_USER -p $ADMIN_PASSWORD -authenticationDatabase admin --eval "print(\"waited for connection\")"
-  do
-    sleep 5
-  done
-
-mongo --host $MONGO_HOST -u $ADMIN_USER -p $ADMIN_PASSWORD -authenticationDatabase admin --eval="db.createUser({user: 'myTester', pwd: 'xyz123', roles: [ { role: 'readWrite', db: 'test' }, { role: 'read', db: 'reporting' } ] } )"
