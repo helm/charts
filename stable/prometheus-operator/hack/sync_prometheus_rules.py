0052489f9ca639f526f57365425dce3d420a9035
Matteo Gazzetta <matteo_gazzetta@hotmail.it>
2019-08-13 00:11:37 +0200

    Sync prometheus operator dashboard and rules (#16230)
    Signed-off-by: Matteo Gazzetta <matteo_gazzetta@hotmail.it>

--- stable/prometheus-operator/hack/sync_prometheus_rules.py
+++ stable/prometheus-operator/hack/sync_prometheus_rules.py
@@ -26,11 +26,27 @@ def change_style(style, representer):
 charts = [
     {
         'source': 'https://raw.githubusercontent.com/coreos/kube-prometheus/master/manifests/prometheus-rules.yaml',
-        'destination': '../templates/prometheus/rules'
+        'destination': '../templates/prometheus/rules-1.14',
+        'min_kubernetes': '1.14.0',
+        'max_kubernetes': '1.16.0'
     },
     {
         'source': 'https://raw.githubusercontent.com/etcd-io/etcd/master/Documentation/op-guide/etcd3_alert.rules.yml',
-        'destination': '../templates/prometheus/rules'
+        'destination': '../templates/prometheus/rules-1.14',
+        'min_kubernetes': '1.14.0',
+        'max_kubernetes': '1.16.0'
+    },
+    {
+        'source': 'https://raw.githubusercontent.com/coreos/kube-prometheus/release-0.1/manifests/prometheus-rules.yaml',
+        'destination': '../templates/prometheus/rules',
+        'min_kubernetes': '1.11.0',
+        'max_kubernetes': '1.14.0'
+    },
+    {
+        'source': 'https://raw.githubusercontent.com/etcd-io/etcd/master/Documentation/op-guide/etcd3_alert.rules.yml',
+        'destination': '../templates/prometheus/rules',
+        'min_kubernetes': '1.11.0',
+        'max_kubernetes': '1.14.0'
     },
 ]
 
@@ -48,6 +64,8 @@ condition_map = {
     'kubernetes-storage': ' .Values.defaultRules.rules.kubernetesStorage',
     'kubernetes-system': ' .Values.defaultRules.rules.kubernetesSystem',
     'node.rules': ' .Values.nodeExporter.enabled .Values.defaultRules.rules.node',
+    'node-network': ' .Values.defaultRules.rules.network',
+    'node-time': ' .Values.defaultRules.rules.time',
     'prometheus-operator': ' .Values.defaultRules.rules.prometheusOperator',
     'prometheus.rules': ' .Values.defaultRules.rules.prometheus',
     'kubernetes-apps': ' .Values.kubeStateMetrics.enabled .Values.defaultRules.rules.kubernetesApps',
@@ -88,7 +106,7 @@ replacement_map = {
 header = '''# Generated from '%(name)s' group from %(url)s
 # Do not change in-place! In order to change this file first read following link:
 # https://github.com/helm/charts/tree/master/stable/prometheus-operator/hack
-{{- if and .Values.defaultRules.create%(condition)s }}%(init_line)s
+{{- if and (semverCompare ">=%(min_kubernetes)s" .Capabilities.KubeVersion.GitVersion) (semverCompare "<%(max_kubernetes)s" .Capabilities.KubeVersion.GitVersion) .Values.defaultRules.create%(condition)s }}%(init_line)s
 apiVersion: {{ printf "%%s/v1" (.Values.prometheusOperator.crdApiGroup | default "monitoring.coreos.com") }}
 kind: PrometheusRule
 metadata:
@@ -173,7 +191,7 @@ def add_rules_conditions(rules, indent=4):
     return rules
 
 
-def write_group_to_file(group, url, destination):
+def write_group_to_file(group, url, destination, min_kubernetes, max_kubernetes):
     fix_expr(group['rules'])
 
     # prepare rules string representation
@@ -193,6 +211,8 @@ def write_group_to_file(group, url, destination):
         'url': url,
         'condition': condition_map.get(group['name'], ''),
         'init_line': init_line,
+        'min_kubernetes': min_kubernetes,
+        'max_kubernetes': max_kubernetes
     }
 
     # rules themselves
@@ -228,7 +248,7 @@ def main():
         # etcd workaround, their file don't have spec level
         groups = yaml_text['spec']['groups'] if yaml_text.get('spec') else yaml_text['groups']
         for group in groups:
-            write_group_to_file(group, chart['source'], chart['destination'])
+            write_group_to_file(group, chart['source'], chart['destination'], chart['min_kubernetes'], chart['max_kubernetes'])
     print("Finished")
 
 
