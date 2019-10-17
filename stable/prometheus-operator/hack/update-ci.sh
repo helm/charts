091bc77406b8a8c7e608a08a6ea79be6d07c6665
Vasily Sliouniaev <vasily.sliouniaev@jet.com>
2019-10-17 11:08:39 +0100

    Change node-exporter port to 9101 for tests (#18066)
    Signed-off-by: vas <vasily.sliouniaev@jet.com>
    Signed-off-by: Ganga Mahesh Siddem <gangams@microsoft.com>

--- stable/prometheus-operator/hack/update-ci.sh
+++ stable/prometheus-operator/hack/update-ci.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-sed -e 's/cleanupCustomResource: false/cleanupCustomResource: true/' \
+sed \
+  -e 's/cleanupCustomResource: false/cleanupCustomResource: true/' \
 	-e 's/cleanupCustomResourceBeforeInstall: false/cleanupCustomResourceBeforeInstall: true/' \
-	values.yaml > ci/test-values.yaml
+	-e 's/port: 9100/port: 9101/' \
+	-e 's/targetPort: 9100/targetPort: 9101/' \
+  values.yaml > ci/test-values.yaml
