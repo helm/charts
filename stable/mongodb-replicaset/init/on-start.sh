337338c83e92696ce2d7f07ad260955c78ddef99
Kai Timmer <kai@staffbase.com>
2019-11-06 16:35:35 +0100

    add a doNotInit value that skips the replicaset creation on first start
    Signed-off-by: Kai Timmer <kai@staffbase.com>

--- stable/mongodb-replicaset/init/on-start.sh
+++ stable/mongodb-replicaset/init/on-start.sh
@@ -151,67 +151,71 @@ fi
 
 init_mongod_standalone
 
-log "Peers: ${peers[*]}"
-log "Starting a MongoDB replica"
-mongod --config /data/configdb/mongod.conf --dbpath=/data/db --replSet="$replica_set" --port="${port}" "${auth_args[@]}" --bind_ip=0.0.0.0 2>&1 | tee -a /work-dir/log.txt 1>&2 &
-pid=$!
-trap shutdown_mongo EXIT
+if [[ "${DO_NOT_INIT}" == "true" ]]; then
+    log "Skipping initialization"
+else
+  log "Peers: ${peers[*]}"
+  log "Starting a MongoDB replica"
+  mongod --config /data/configdb/mongod.conf --dbpath=/data/db --replSet="$replica_set" --port="${port}" "${auth_args[@]}" --bind_ip=0.0.0.0 2>&1 | tee -a /work-dir/log.txt 1>&2 &
+  pid=$!
+  trap shutdown_mongo EXIT
 
-log "Waiting for MongoDB to be ready..."
-retry_until "localhost" "db.adminCommand('ping').ok" "1"
-log "Initialized."
+  log "Waiting for MongoDB to be ready..."
+  retry_until "localhost" "db.adminCommand('ping').ok" "1"
+  log "Initialized."
 
-# try to find a master
-for peer in "${peers[@]}"; do
-    log "Checking if ${peer} is primary"
-    # Check rs.status() first since it could be in primary catch up mode which db.isMaster() doesn't show
-    if [[ $(mongo admin --host "${peer}" "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "rs.status().myState") == "1" ]]; then
-        retry_until "${peer}" "db.isMaster().ismaster" "true"
-        log "Found primary: ${peer}"
-        primary="${peer}"
-        break
-    fi
-done
-
-if [[ "${primary}" = "${service_name}" ]]; then
-    log "This replica is already PRIMARY"
-elif [[ -n "${primary}" ]]; then
-    if [[ $(mongo admin --host "${primary}" "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "rs.conf().members.findIndex(m => m.host == '${service_name}:${port}')") == "-1" ]]; then
-      log "Adding myself (${service_name}) to replica set..."
-      if (mongo admin --host "${primary}" "${admin_creds[@]}" "${ssl_args[@]}" --eval "rs.add('${service_name}')" | grep 'Quorum check failed'); then
-          log 'Quorum check failed, unable to join replicaset. Exiting prematurely.'
-          exit 1
+  # try to find a master
+  for peer in "${peers[@]}"; do
+      log "Checking if ${peer} is primary"
+      # Check rs.status() first since it could be in primary catch up mode which db.isMaster() doesn't show
+      if [[ $(mongo admin --host "${peer}" "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "rs.status().myState") == "1" ]]; then
+          retry_until "${peer}" "db.isMaster().ismaster" "true"
+          log "Found primary: ${peer}"
+          primary="${peer}"
+          break
       fi
-    fi
+  done
 
-    sleep 3
-    log 'Waiting for replica to reach SECONDARY state...'
-    retry_until "${service_name}" "rs.status().myState" "2"
-    log '✓ Replica reached SECONDARY state.'
+  if [[ "${primary}" = "${service_name}" ]]; then
+      log "This replica is already PRIMARY"
+  elif [[ -n "${primary}" ]]; then
+      if [[ $(mongo admin --host "${primary}" "${admin_creds[@]}" "${ssl_args[@]}" --quiet --eval "rs.conf().members.findIndex(m => m.host == '${service_name}:${port}')") == "-1" ]]; then
+        log "Adding myself (${service_name}) to replica set..."
+        if (mongo admin --host "${primary}" "${admin_creds[@]}" "${ssl_args[@]}" --eval "rs.add('${service_name}')" | grep 'Quorum check failed'); then
+            log 'Quorum check failed, unable to join replicaset. Exiting prematurely.'
+            exit 1
+        fi
+      fi
 
-elif (mongo "${ssl_args[@]}" --eval "rs.status()" | grep "no replset config has been received"); then
-    log "Initiating a new replica set with myself ($service_name)..."
-    mongo "${ssl_args[@]}" --eval "rs.initiate({'_id': '$replica_set', 'members': [{'_id': 0, 'host': '$service_name'}]})"
+      sleep 3
+      log 'Waiting for replica to reach SECONDARY state...'
+      retry_until "${service_name}" "rs.status().myState" "2"
+      log '✓ Replica reached SECONDARY state.'
 
-    sleep 3
-    log 'Waiting for replica to reach PRIMARY state...'
-    retry_until "localhost" "db.isMaster().ismaster" "true"
-    primary="${service_name}"
-    log '✓ Replica reached PRIMARY state.'
+  elif (mongo "${ssl_args[@]}" --eval "rs.status()" | grep "no replset config has been received"); then
+      log "Initiating a new replica set with myself ($service_name)..."
+      mongo "${ssl_args[@]}" --eval "rs.initiate({'_id': '$replica_set', 'members': [{'_id': 0, 'host': '$service_name'}]})"
 
-    if [[ "${AUTH}" == "true" ]]; then
-        log "Creating admin user..."
-        mongo admin "${ssl_args[@]}" --eval "db.createUser({user: '${admin_user}', pwd: '${admin_password}', roles: [{role: 'root', db: 'admin'}]})"
-    fi
-fi
+      sleep 3
+      log 'Waiting for replica to reach PRIMARY state...'
+      retry_until "localhost" "db.isMaster().ismaster" "true"
+      primary="${service_name}"
+      log '✓ Replica reached PRIMARY state.'
 
-# User creation
-if [[ -n "${primary}" && "$AUTH" == "true" && "$METRICS" == "true" ]]; then
-    metric_user_count=$(mongo admin --host "${primary}" "${admin_creds[@]}" "${ssl_args[@]}" --eval "db.system.users.find({user: '${metrics_user}'}).count()" --quiet)
-    if [[ "${metric_user_count}" == "0" ]]; then
-        log "Creating clusterMonitor user..."
-        mongo admin --host "${primary}" "${admin_creds[@]}" "${ssl_args[@]}" --eval "db.createUser({user: '${metrics_user}', pwd: '${metrics_password}', roles: [{role: 'clusterMonitor', db: 'admin'}, {role: 'read', db: 'local'}]})"
-    fi
+      if [[ "${AUTH}" == "true" ]]; then
+          log "Creating admin user..."
+          mongo admin "${ssl_args[@]}" --eval "db.createUser({user: '${admin_user}', pwd: '${admin_password}', roles: [{role: 'root', db: 'admin'}]})"
+      fi
+  fi
+
+  # User creation
+  if [[ -n "${primary}" && "$AUTH" == "true" && "$METRICS" == "true" ]]; then
+      metric_user_count=$(mongo admin --host "${primary}" "${admin_creds[@]}" "${ssl_args[@]}" --eval "db.system.users.find({user: '${metrics_user}'}).count()" --quiet)
+      if [[ "${metric_user_count}" == "0" ]]; then
+          log "Creating clusterMonitor user..."
+          mongo admin --host "${primary}" "${admin_creds[@]}" "${ssl_args[@]}" --eval "db.createUser({user: '${metrics_user}', pwd: '${metrics_password}', roles: [{role: 'clusterMonitor', db: 'admin'}, {role: 'read', db: 'local'}]})"
+      fi
+  fi
 fi
 
 log "MongoDB bootstrap complete"
