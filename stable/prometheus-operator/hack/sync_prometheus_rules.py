13ea3156b4ded27e72cf9dfb4a9b92f0479bcd69
iuri aranda <iuri.aranda@gmail.com>
2020-04-02 15:44:24 +0200

    [stable/prometheus-operator] Re-sync prometheus rules and grafana dashboards (#21248)
    * Re-sync prometheus rules
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    
    * Manually fix special notiation in prometheus rules
    
    I see that this was previously manually fixed as well, so I kept it like before
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    
    * Bump chart version
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    
    * Bump patch version
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    
    * Fix Helm interpolation escaping in sync scripts
    
    This should fix the issue of having a backtick (`) inside a {{ ... }} command block in prometheus syntax.
    
    As per https://github.com/helm/charts/pull/21248#issuecomment-595725478
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    
    * Bump patch version
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    
    * Remove unnecessary whitespace
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    
    * Bump patch version
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    
    * Bump prometheus-operator chart patch version
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    
    * Update prometheus rules gain
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    
    * Sync grafana dashboards as well
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    
    * Bump patch version for prometheus-operator chart
    
    Signed-off-by: iuri aranda <iuri@skyscrapers.eu>
    sign: Dmitry Kozlov <dmitry.f.kozlov@gmail.com>

--- stable/prometheus-operator/hack/sync_prometheus_rules.py
+++ stable/prometheus-operator/hack/sync_prometheus_rules.py
@@ -120,7 +120,7 @@ replacement_map = {
 }
 
 # standard header
-header = '''{{- /* 
+header = '''{{- /*
 Generated from '%(name)s' group from %(url)s
 Do not change in-place! In order to change this file first read following link:
 https://github.com/helm/charts/tree/master/stable/prometheus-operator/hack
@@ -153,7 +153,7 @@ def init_yaml_styles():
 
 
 def escape(s):
-    return s.replace("{{", "{{`{{").replace("}}", "}}`}}")
+    return s.replace("{{", "{{`{{").replace("}}", "}}`}}").replace("{{`{{", "{{`{{`}}").replace("}}`}}", "{{`}}`}}")
 
 
 def fix_expr(rules):
